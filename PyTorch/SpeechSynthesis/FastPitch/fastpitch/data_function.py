ac7ae6c0d59e5904ff5cbf02c65c427788708b2d
Adyatma <awan2403@gmail.com>
2024-07-19 17:22:01 +0700

    debug sampling

--- PyTorch/SpeechSynthesis/FastPitch/fastpitch/data_function.py
+++ PyTorch/SpeechSynthesis/FastPitch/fastpitch/data_function.py
@@ -237,7 +237,7 @@ class TTSDataset(torch.utils.data.Dataset):
             if sampling_rate != self.stft.sampling_rate:
                 # resampler = torchaudio.transforms.Resample(orig_freq=sampling_rate, new_freq=self.stft.sampling_rate)
                 # audio = resampler(audio)
-                print(audio.size(1))
+                print(len(audio))
                 num_samples = int(audio.size(1) * self.stft.sampling_rate / sampling_rate)
                 audio = signal.resample(audio.numpy(), num_samples)
                 audio = torch.from_numpy(audio)
