8b0060029ec00cd3b3819f532e259195e0ad0631
Adyatma <awan2403@gmail.com>
2024-07-19 16:48:31 +0700

    Add start train script for female javanese TTS

---
+++ PyTorch/SpeechSynthesis/FastPitch/scripts/jv_fe/train.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -a
+
+PYTHONIOENCODING=utf-8
+
+# Mandarin & English bilingual
+ARGS+=" --symbol-set english_basic"
+
+# Initialize weights with a pre-trained English model
+bash scripts/download_models.sh fastpitch
+ARGS+=" --init-from-checkpoint pretrained_models/fastpitch/nvidia_fastpitch_210824.pt"
+
+AMP=false  # FP32 training for better stability
+
+: ${DATASET_PATH:=data/jv_id_female}
+: ${TRAIN_FILELIST:=filelists/jv_fe_audio_pitch_text_train.txt}
+: ${VAL_FILELIST:=filelists/jv_fe_audio_pitch_text_val.txt}
+: ${OUTPUT_DIR:=./output_jv_fe}
+
+bash scripts/train.sh $ARGS "$@"
