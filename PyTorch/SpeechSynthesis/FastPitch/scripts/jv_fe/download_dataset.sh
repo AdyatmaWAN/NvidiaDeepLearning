fd22a404661e719ee62baed1c472bb593e9cb5b5
Adyatma <awan2403@gmail.com>
2024-07-29 08:57:18 +0700

    add script for downloading javanese female TTS dataset

---
+++ PyTorch/SpeechSynthesis/FastPitch/scripts/jv_fe/download_dataset.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+scripts/download_cmudict.sh
+
+DATA_DIR="data/java_id_female"
+#LJS_ARCH="LJSpeech-1.1.tar.bz2"
+#LJS_URL="http://data.keithito.com/data/speech/${LJS_ARCH}"
+
+if [ ! -d ${DATA_DIR} ]; then
+  echo "Downloading java_id_female ..."
+  wget -q https://www.openslr.org/resources/41/jv_id_female.zip
+  echo "Extracting java_id_female ..."
+  unzip jxvf java_id_female.zip
+  rm -f java_id_female.zip
+fi
