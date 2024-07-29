3c60dd42d4bf5b82a16871f0483e8f6fbe3c0988
Adyatma <awan2403@gmail.com>
2024-07-29 08:55:45 +0700

    Change line separator for CRLF to LF

--- PyTorch/SpeechSynthesis/FastPitch/scripts/docker/interactive.sh
+++ PyTorch/SpeechSynthesis/FastPitch/scripts/docker/interactive.sh
@@ -3,3 +3,5 @@
 PORT=${PORT:-8888}
 
 docker run --gpus=all -it --rm -e CUDA_VISIBLE_DEVICES --ipc=host -p $PORT:$PORT -v $PWD:/workspace/fastpitch/ fastpitch:latest bash 
+
+#docker run --gpus=all -it --rm -e CUDA_VISIBLE_DEVICES --ipc=host -p 8888:8888 -v ${PWD}:/workspace/fastpitch/ fastpitch:latest bash
